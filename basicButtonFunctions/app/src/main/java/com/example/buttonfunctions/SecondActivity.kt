103cff3ec328ba89afd9bc4a19126682c96f0b3f
Symonn Ang <symonnjaemur29@gmail.com>
2025-09-05 19:03:35 +0800

    add codes, bg to 2nd xml

--- basicButtonFunctions/app/src/main/java/com/example/buttonfunctions/SecondActivity.kt
+++ basicButtonFunctions/app/src/main/java/com/example/buttonfunctions/SecondActivity.kt
@@ -2,10 +2,15 @@ package com.example.buttonfunctions
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import android.graphics.Color
+
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val bgImage = findViewById<ImageView>(R.id.imageView3)
+        bgImage.setColorFilter(Color.parseColor("#80000000"))
     }
 }
