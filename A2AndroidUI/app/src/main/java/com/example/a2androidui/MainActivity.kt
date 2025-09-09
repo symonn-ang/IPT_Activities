e7ab0de91a0adce036185ef68c440a39b2c2156b
Symonn Ang <symonnjaemur29@gmail.com>
2025-09-09 19:04:11 +0800

    add new dir for act 2, constraints still broken

---
+++ A2AndroidUI/app/src/main/java/com/example/a2androidui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.a2androidui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.text.HtmlCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+            val textView = findViewById<TextView>(R.id.myTextView)
+
+            val htmlText = getString(R.string.welcome_text)
+            textView.text = HtmlCompat.fromHtml(htmlText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        }
+    }
+}
