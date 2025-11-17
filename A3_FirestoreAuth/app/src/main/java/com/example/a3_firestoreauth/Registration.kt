0ccfc4595f55d33c7f2303a01b558678e8d1787d
Symonn Ang <symonnjaemur29@gmail.com>
2025-11-17 12:28:36 +0800

    add loginregister auths, update old files

--- A3_FirestoreAuth/app/src/main/java/com/example/a3_firestoreauth/Registration.kt
+++ A3_FirestoreAuth/app/src/main/java/com/example/a3_firestoreauth/Registration.kt
@@ -1,27 +1,28 @@
 package com.example.a3_firestoreauth
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
+import android.widget.Button
+import android.widget.ProgressBar
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import android.content.Intent
+class Registration : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
     private lateinit var editTextEmail: TextInputEditText
     private lateinit var editTextPassword: TextInputEditText
-    private lateinit var buttonLog: Button
+    private lateinit var buttonReg: Button
     private lateinit var auth: FirebaseAuth
     private lateinit var progBar: ProgressBar
-    private lateinit var regButton: Button
+    private lateinit var backButton: Button
 
     public override fun onStart() {
         super.onStart()
@@ -37,26 +38,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registration)
+        auth = FirebaseAuth.getInstance();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
-        auth = FirebaseAuth.getInstance();
         editTextEmail = findViewById(R.id.username)
         editTextPassword = findViewById(R.id.password)
-        buttonLog = findViewById(R.id.loginBtn)
+        buttonReg = findViewById(R.id.registerBtn)
         progBar = findViewById(R.id.progressBar)
-        regButton = findViewById(R.id.registerBtn)
-        regButton.setOnClickListener {
-
-            val intent = Intent(this, Registration::class.java)
+        backButton = findViewById(R.id.backBtn)
+        backButton.setOnClickListener {
+            // Create an Intent to go back to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            finish() // optional, back or none ;3
+            finish() // optional: closes the current activity so user can't press back to return here
         }
 
-        buttonLog.setOnClickListener {
+        buttonReg.setOnClickListener {
             progBar.setVisibility(View.VISIBLE);
             val email = editTextEmail.text.toString().trim()
             val password = editTextPassword.text.toString().trim()
@@ -71,30 +73,22 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            auth.signInWithEmailAndPassword(email, password)
+            auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
-                    progBar.setVisibility(View.GONE); // don't forget
+                    progBar.setVisibility(View.GONE);
                     if (task.isSuccessful) {
-                        // sign in success
-                        Log.d("LoginActivity", "signInWithEmail:success")
-                        val user = auth.currentUser
-//                        updateUI(user) custom apparently maybe touch later?
-                        val intent = Intent(this, Home::class.java)
-                        startActivity(intent)
-                        finish()
+                        // Sign in success
+                        Log.d("Registration", "createUserWithEmail:success")
+                        val user: FirebaseUser? = auth.currentUser
+//                        updateUI(user)
                     } else {
-                        // sign in fails
-                        Log.w("LoginActivity", "signInWithEmail:failure", task.exception)
-                        Toast.makeText(
-                            this, // 'this' refers to the Activity context
-                            "Authentication failed.",
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        // Sign in fails
+                        Log.w("Registration", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
 //                        updateUI(null)
                     }
                 }
         }
 
-        // bottom
     }
 }
